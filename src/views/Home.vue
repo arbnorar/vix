836e5782d9348054145318f16c19fb459388aa18
Laris Mahmutaj <laris.mahmutaj@gmail.com>
2020-05-22 23:35:06 +0200

    Landing Page done
    Co-authored-by: Arbnora Reshani <arbnorareshani@gmail.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<div id="nav">
+			<router-link to="/home">Home</router-link> |
+			<router-link to="/about">About</router-link>
+		</div>
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+	// @ is an alias to /src
+	import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: "Home",
+		components: {
+			HelloWorld
+		}
+	};
 </script>
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,79 @@
+<template>
+	<div>
+		<video
+			src="../assets/videos/wallpaper-video.mp4"
+			poster="images/snapshot.PNG"
+			id="videoBackground"
+			video
+			loop
+			muted
+			autoplay
+			controls
+		>
+			This is fallback content to display for user agents that do not support
+			the video tag.
+		</video>
+		<div class="text">
+			<h1>Noah's Ark</h1>
+			<p>
+				There are no unwanted pets.. &nbsp; &nbsp; &nbsp;just unfound families.
+			</p>
+			<router-link to="/home" class="btn btn-success">Home</router-link>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "LandingPage"
+	};
+</script>
+
+<style scoped>
+	body {
+		margin: 0;
+	}
+
+	#videoBackground {
+		object-fit: cover;
+		width: 100vw;
+		height: 100vh;
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: -1;
+	}
+
+	#videoBackground::-webkit-media-controls {
+		display: none;
+	}
+
+	h1 {
+		font-family: Verdana, Geneva, Tahoma, sans-serif;
+		font-size: 55px;
+		color: white;
+		text-shadow: 2px 2px black;
+	}
+
+	p {
+		font-family: Verdana, Geneva, Tahoma, sans-serif;
+		font-size: 30px;
+		color: white;
+		text-shadow: 1px 1px black;
+	}
+
+	button {
+		width: fit-content;
+	}
+
+	.text {
+		max-width: 450px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		align-self: flex-end;
+		margin-top: 250px;
+		margin-right: 70px;
+		margin-left: auto;
+	}
+</style>
