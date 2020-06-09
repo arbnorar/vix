a494a391871f61bbb93d3f00b022b2d705da0b4e
Laris Mahmutaj <laris.mahmutaj@gmail.com>
2020-06-09 18:28:34 +0200

    Carousel on Home view

---
+++ src/components/Carousel.vue
@@ -0,0 +1,51 @@
+<template>
+	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img src="@/assets/images/lynx.jpg" class="d-block w-100" />
+			</div>
+			<div class="carousel-item">
+				<img src="@/assets/images/giraffe.jpg" class="d-block w-100" />
+			</div>
+			<div class="carousel-item">
+				<img src="@/assets/images/deer.jpg" class="d-block w-100" />
+			</div>
+			<div class="carousel-item">
+				<img src="@/assets/images/puma.jpg" class="d-block w-100" />
+			</div>
+			<div class="carousel-item">
+				<img src="@/assets/images/wolf.jpg" class="d-block w-100" />
+			</div>
+		</div>
+		<a
+			class="carousel-control-prev"
+			href="#carouselExampleControls"
+			role="button"
+			data-slide="prev"
+		>
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a
+			class="carousel-control-next"
+			href="#carouselExampleControls"
+			role="button"
+			data-slide="next"
+		>
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Carousel"
+	};
+</script>
+
+<style scoped>
+	.carousel {
+		width: 70%;
+	}
+</style>
---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="navbar">
+		<img src="@/assets/images/shywolf-logo.png" />
+		<router-link to="/home" style="color: black;">Home</router-link>
+		<router-link to="/about" style="color: black;">About</router-link>
+		<router-link to="/animals" style="color: black;">Animals</router-link>
+		<router-link to="/education" style="color: black;">Education</router-link>
+		<router-link to="/volunteer" style="color: black;">Volunteer</router-link>
+		<router-link to="/connect" style="color: black;">Connect</router-link>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Navbar",
+		data() {
+			return {};
+		}
+	};
+</script>
+
+<style>
+</style>
