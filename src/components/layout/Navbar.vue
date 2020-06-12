ac83bb8b80f6755f7d3ac2c1bfea402550fdc114
Aria <arbnorareshani@gmail.com>
2020-06-12 04:11:14 +0200

    edited navbar

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,73 +1,79 @@
 <template>
-	<nav class="navbar navbar-light navbar-expand-lg">
-		<router-link to="/home" class="navbar-brand">
-			<img src="@/assets/images/mainlogo.png" height="70px" />
-			<br />Wildlife Center
-		</router-link>
-		<button
-			class="navbar-toggler"
-			type="button"
-			data-toggle="collapse"
-			data-target="#navbarSupportedContent"
-			aria-controls="navbarSupportedContent"
-			aria-expanded="false"
-			aria-label="Toggle navigation"
-		>
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav ml-auto mr-5 w-75">
-				<li class="nav-item">
-					<router-link to="/home" class="nav-link h4">Home</router-link>
-				</li>
-				<li class="nav-item two">
-					<router-link to="/about" class="nav-link h4">About</router-link>
-				</li>
-				<li class="nav-item three">
-					<router-link to="/animals" class="nav-link h4">Animals</router-link>
-				</li>
-				<li class="nav-item four">
-					<router-link to="/education" class="nav-link h4"
-						>Education</router-link
-					>
-				</li>
-				<li class="nav-item five">
-					<router-link to="/volunteer" class="nav-link h4"
-						>Volunteer</router-link
-					>
-				</li>
-				<li class="nav-item six">
-					<router-link to="/connect" class="nav-link h4">Connect</router-link>
-				</li>
-			</ul>
-		</div>
-	</nav>
+  <nav class="navbar navbar-light navbar-expand-lg">
+    <router-link to="/home" class="navbar-brand">
+      <img src="@/assets/images/mainlogo.png" height="70px" />
+      <br />Wildlife Center
+    </router-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mr-5 w-75">
+        <li class="nav-item">
+          <router-link to="/home" class="nav-link h4">Home</router-link>
+        </li>
+        <li class="nav-item two">
+          <router-link to="/about" class="nav-link h4">About</router-link>
+        </li>
+        <li class="nav-item three">
+          <router-link to="/animals" class="nav-link h4">Animals</router-link>
+        </li>
+        <li class="nav-item four">
+          <router-link to="/education" class="nav-link h4">Education</router-link>
+        </li>
+        <li class="nav-item five">
+          <router-link to="/volunteer" class="nav-link h4">Volunteer</router-link>
+        </li>
+        <li class="nav-item six">
+          <router-link to="/connect" class="nav-link h4">Connect</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
-	export default {
-		name: "Navbar",
-		data() {
-			return {};
-		}
-	};
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style scoped>
-	.navbar-brand {
-		font-size: 26px;
-		color: #1d3a1c;
-	}
-	.navbar-nav {
-		display: flex;
-		justify-content: space-between;
-	}
+.navbar-brand {
+  font-size: 26px;
+  color: #1d3a1c;
+}
+.navbar-nav {
+  display: flex;
+  justify-content: space-between;
+}
 
-	.nav-link {
-		color: black !important;
-	}
+.nav-link {
+  color: black !important;
+}
 
-	.nav-link:hover {
-		text-decoration: 2px underline;
-	}
+.nav-link::after {
+  content: "";
+  display: block;
+  width: 0;
+  height: 2px;
+  background: black;
+  transition: width 0.3s;
+}
+
+.nav-link:hover::after {
+  width: 100%;
+  transition: width 0.5s;
+}
 </style>
