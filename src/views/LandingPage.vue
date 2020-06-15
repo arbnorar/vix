8a78f66dfe438e07b3caa34ad12cfc1434d74d16
Laris Mahmutaj <laris.mahmutaj@gmail.com>
2020-06-15 18:46:15 +0200

    Landing page video blur
    Co-authored-by: Arbnora Reshani <arbnorareshani@gmail.com>

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,82 +1,85 @@
 <template>
-  <div>
-    <video
-      src="../assets/videos/buddy.mp4"
-      poster="images/snapshot.PNG"
-      id="videoBackground"
-      video
-      loop
-      muted
-      autoplay
-      controls
-    >
-      This is fallback content to display for user agents that do not support
-      the video tag.
-    </video>
-    <div class="text">
-      <h1>Vix Wildlife Center</h1>
-      <p>There are no unwanted pets.. &nbsp; &nbsp; &nbsp;just unfound families.</p>
-      <router-link to="/home" class="btn btn-success">Home</router-link>
-    </div>
-  </div>
+	<div class="showcase">
+		<div class="video-container">
+			<video
+				src="../assets/videos/buddy.mp4"
+				poster="images/snapshot.PNG"
+				id="videoBackground"
+				video
+				loop
+				muted
+				autoplay
+			>
+				This is fallback content to display for user agents that do not support
+				the video tag.
+			</video>
+		</div>
+		<div class="content">
+			<h1>Vix Wildlife Center</h1>
+			<p>
+				There are no unwanted pets.. &nbsp; &nbsp; &nbsp;just unfound families.
+			</p>
+			<router-link to="/home" class="btn btn-success">Home</router-link>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "LandingPage"
-};
+	export default {
+		name: "LandingPage"
+	};
 </script>
 
 <style scoped>
-body {
-  margin: 0;
-}
+	body {
+		margin: 0;
+	}
 
-#videoBackground {
-  object-fit: cover;
-  width: 100vw;
-  height: 100vh;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -1;
-  background: rgba(0, 0, 0, 0.4);
-}
+	.showcase {
+		position: relative;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		padding: 0 20px;
+	}
 
-#videoBackground::-webkit-media-controls {
-  display: none !important;
-}
+	.video-container {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
+		color: white;
+	}
 
-#videoBackground::-webkit-media-controls-enclosure {
-  display: none !important;
-}
+	.content {
+		z-index: 10;
+		color: white;
+	}
 
-h1 {
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
-  font-size: 55px;
-  color: white;
-  text-shadow: 2px 2px black;
-}
+	.video-container video {
+		min-width: 100%;
+		min-height: 100%;
+		object-fit: cover;
+		position: absolute;
+		top: 50%;
+		left: 70%;
+		transform: translate(-70%, -50%);
+		filter: blur(4px);
+		-webkit-filter: blur(4px);
+	}
 
-p {
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
-  font-size: 30px;
-  color: white;
-  text-shadow: 1px 1px black;
-}
-
-button {
-  width: fit-content;
-}
-
-.text {
-  max-width: 450px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  align-self: flex-end;
-  margin-top: 250px;
-  margin-right: 70px;
-  margin-left: auto;
-}
+	.video-container:after {
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.5);
+	}
 </style>
